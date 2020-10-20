bfef284b874da46b285662958bb72a3ac1e3d447
Juan Andres Vico <juanandresvico@hotmail.com>
2020-10-20 02:26:08 -0300

    Authorization (#15)

--- lib/guardian_web/views/admin/error_view.ex
+++ lib/guardian_web/views/admin/error_view.ex
@@ -4,6 +4,11 @@ defmodule GuardianWeb.Admin.ErrorView do
   import Torch.TableView
   import Torch.FilterView
 
+  alias Guardian.Accounts.User
+
   def show_assignee(nil), do: "No assignee"
   def show_assignee(assignee), do: assignee.name
+
+  def admin?(%User{role: :admin}), do: true
+  def admin?(_), do: false
 end
--- lib/guardian_web/views/layout_view.ex
+++ lib/guardian_web/views/layout_view.ex
@@ -1,3 +1,8 @@
 defmodule GuardianWeb.LayoutView do
   use GuardianWeb, :view
+
+  alias Guardian.Accounts.User
+
+  def admin?(%User{role: :admin}), do: true
+  def admin?(_), do: false
 end
