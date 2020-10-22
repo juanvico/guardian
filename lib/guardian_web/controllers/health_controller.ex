d92fdc1147aa528f4bf3cbe9a20c1743a18dc773
Juan A. Vico <juanandresvico@hotmail.com>
2020-10-22 18:29:19 -0300

    Fixes

--- lib/guardian_web/controllers/health_controller.ex
+++ lib/guardian_web/controllers/health_controller.ex
@@ -2,6 +2,6 @@ defmodule GuardianWeb.HealthController do
   use GuardianWeb, :controller
 
   def show(conn, _params) do
-    send_resp(conn, 200, "ok")
+    send_resp(conn, 200, "Database STATUS: OK \nRequests STATUS: OK \n")
   end
 end
