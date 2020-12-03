baaaedb18e936b490815b58efba88316670b992e
flomosq <florenciamosqueraa@gmail.com>
2020-12-03 19:18:24 -0300

    Created statistics project

---
+++ statistics/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const errorsRouter = require('./errors');
+const router = express.Router();
+
+router.get('/', (_req, res) => {
+  res.send('Statistics API');
+});
+router.use('/errors', errorsRouter);
+
+module.exports = router;
