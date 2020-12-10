63382c4a3440967cbf9cfad8fa2b9f25e118eb84
flomosq <florenciamosqueraa@gmail.com>
2020-12-10 15:30:37 -0300

    Added connection test to library

--- library/index.js
+++ library/index.js
@@ -6,14 +6,21 @@ let apiKey = '';
 const init = ({ key }) => {
   apiKey = key;
   httpClient = axios.create({
-    baseURL: 'http://localhost:4000/api/v1',
-    timeout: 1000,
+    baseURL: 'http://localhost:4000',
+    timeout: 500,
     headers: { 'application-key': apiKey },
   });
+  return testConnection();
+};
+
+const testConnection = () => {
+  return httpClient.get('/health')
+    .then(() => true)
+    .catch(() => 'Connection to Centinela failed');
 };
 
 const createError = ({ title, assignedDeveloper, description, severity }) => {
-  return httpClient.post('/errors', { error: { title, assignedDeveloper, description, severity } });
+  return httpClient.post('/api/v1/errors', { error: { title, assignedDeveloper, description, severity } });
 };
 
 module.exports = {
