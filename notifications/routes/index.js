9336a67d2d3fb9dc9363a17f7c690c6229cc4177
flomosq <florenciamosqueraa@gmail.com>
2020-12-10 14:57:43 -0300

    Removed deprectated endpoints from notifications MS

--- notifications/routes/index.js
+++ notifications/routes/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const errorsRouter = require('./errors');
 const usersRouter = require('./users');
 
 const router = express.Router();
@@ -8,7 +7,6 @@ router.get('/', (_req, res) => {
   res.send('Notifications API');
 });
 
-router.use('/errors', errorsRouter);
 router.use('/users', usersRouter);
 
 module.exports = router;
